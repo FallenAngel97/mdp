eacda67eaeba6cff321275e1bf6eac7b6c5fee10
Lex <mr.x97@mail.ru>
2021-04-12 20:41:47 +0300

    Added the dependency fetching for building the wheels for Python

--- machine_diagnostics_protocol/MDPServer.test.py
+++ machine_diagnostics_protocol/MDPServer.test.py
@@ -1,7 +1,6 @@
 import unittest
 import MDPServer
 
-
 class MDPClientTest(unittest.TestCase):
     def test_generating_strings(self):
         self.assertEqual(len(MDPServer.MDPServer().random_string(4)), 4)
